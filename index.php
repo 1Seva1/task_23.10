593eda1cf0db2f14c8455e9ac9273046da5a1f24
1Seva1 <sevostyanovmv@gmail.com>
2022-09-21 11:36:58 +0300

    edit index.php

--- index.php
+++ index.php
@@ -2,25 +2,36 @@
 
 abstract class technique {
 abstract public function Run();
+abstract public function specAction();
 }
 
 class Car extends technique {
     public function Run(){
-        echo 'Машина поехала';
+        echo 'Машина поехала'."\r\n";
+    }
+    public function specAction(){
+        echo 'Закись азота';
     }
 }
 class Tanks extends technique {
     public function Run(){
-        echo 'Танк поехал';
+        echo 'Танк поехал'."\r\n";
+    }
+    public function specAction(){
+        echo 'Выстрел из пушки';
     }
 }
 class specEquip extends technique {
     public function Run(){
-        echo 'Бульдозер поехал';
+        echo 'Бульдозер поехал'."\r\n";
+    }
+    public function specAction(){
+        echo 'Ковш поднят';
     }
 }
 $car = new Car();
 function enginery(technique $car) {
     $car->Run();
+    $car->specAction();
   }
 enginery($car);
